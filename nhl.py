5e25bcaa58fcad0dd4c998b682885ee2cf6abc3d
kaa090 <49017533+kaa090@users.noreply.github.com>
2022-09-21 02:59:33 +0300

    Update nhl.py

--- nhl.py
+++ nhl.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import requests
-import jmespath as j
 import pandas as pd
 from time import sleep
 
 season = "20212022"
-stats = {}
+statsP = {}
+statsG = {}
+keysP = ['goals', 'assists', 'pim', 'powerPlayPoints', 'gameWinningGoals', 'shots', 'hits', 'blocked',]
+keysG = ['wins', 'goalAgainstAverage', 'savePercentage']
 
 teams = requests.get(f"https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster").json()
 
@@ -17,34 +19,54 @@ for team in teams['teams']:
 
 	try:
 		for player in players:
+			pid = player['person']['id']
+			name = player['person']['fullName']
+			name2 = player['person']['fullName'].split(' ')
+			name2 = f"{name2[1]}, {name2[0]}"
+			link = player['person']['link']
+			pos = player['position']['abbreviation']
+			
 
-			player_id = player['person']['id']
-			player_name = player['person']['fullName']
-			player_name2 = player['person']['fullName'].split(' ')
-			player_name2 = f"{player_name2[1]}, {player_name2[0]}"
-			player_link = player['person']['link']
-			player_pos = player['position']['abbreviation']
+			my_statP = dict.fromkeys(keysP, 0)
+			my_statG = dict.fromkeys(keysG, 0)
 
-			print(player_name2)
+			print(name2)
 
-			player_data = requests.get(f"https://statsapi.web.nhl.com{player_link}/stats?stats=statsSingleSeason&season={season}").json()
-			player_stats = player_data['stats'][0]['splits'][0]['stat']
-			player_sum = 0
+			player_data = requests.get(f"https://statsapi.web.nhl.com{link}/stats?stats=statsSingleSeason&season={season}").json()
+			
+			if player_data['stats'][0]['splits']:
+				stat = player_data['stats'][0]['splits'][0]['stat']				
 
-			if len(player_stats) > 0:
-				if player_pos == 'G':
-					player_sum = player_stats['wins'] + player_stats['goalAgainstAverage'] + player_stats['savePercentage']
+				if pos == 'G':
+					for key in keysG:
+						if key in stat:
+							my_statG[key] = stat[key]
+					my_statG.update(stat)
+					statsG[pid] = [name, name2, pos, team_name, my_statG]
 				else:
-					player_sum = player_stats['goals'] + player_stats['assists'] + player_stats['pim'] + player_stats['powerPlayPoints'] + player_stats['gameWinningGoals'] + player_stats['shots'] + player_stats['hits'] + player_stats['blocked']
-				stats[player_id] = [player_name, player_name2, player_pos, player_sum, player_stats]
-			else:
-				stats[player_id] = [player_name, player_name2, player_pos, player_sum, {}]
+					for key in keysP:
+						if key in stat:
+							my_statP[key] = stat[key]
+					my_statP.update(stat)
+					statsP[pid] = [name, name2, pos, team_name, my_statP]
 	except Exception as e:
 		print(str(e))
-		print(players)
 
-df = pd.DataFrame.from_dict(stats, orient="index", columns=['name', 'name2', 'pos', 'sum', 'stats'])
-df1 = df.reset_index()
-df2 = pd.json_normalize(df['stats'].dropna())
-df3 = pd.merge(df1, df2, left_index=True, right_index=True).set_index('index').drop('stats', 1)
-df3.to_csv(f"nhlstats{season}.csv")
+dfP = pd.DataFrame.from_dict(statsP, orient="index", columns=['name', 'name2', 'pos', 'team', 'stats'])
+dfP1 = dfP.reset_index()
+dfP2 = pd.json_normalize(dfP['stats'].dropna())
+dfP3 = pd.merge(dfP1, dfP2, left_index=True, right_index=True).set_index('index').drop('stats', 1)
+dfP3[keysP] = dfP3[keysP].apply(lambda x: (x - x.mean()) / x.std() )
+dfP3.insert(4, 'sum', 0)
+dfP3['sum'] = dfP3['goals'] + dfP3['assists']  + dfP3['pim'] + dfP3['powerPlayPoints'] 
++ dfP3['gameWinningGoals'] + dfP3['shots'] + dfP3['hits'] + dfP3['blocked']
+dfP3.to_csv(f"nhl_statsP_{season}.csv")
+
+dfG = pd.DataFrame.from_dict(statsG, orient="index", columns=['name', 'name2', 'pos', 'team', 'stats'])
+dfG1 = dfG.reset_index()
+dfG2 = pd.json_normalize(dfG['stats'].dropna())
+dfG3 = pd.merge(dfG1, dfG2, left_index=True, right_index=True).set_index('index').drop('stats', 1)
+dfG3[keysG] = dfG3[keysG].apply(lambda x: (x - x.mean()) / x.std() )
+dfG3.insert(4, 'sum', 0)
+dfG3['sum'] = dfG3['wins'] + dfG3['goalAgainstAverage']  + dfG3['savePercentage'] 
+dfG3.to_csv(f"nhl_statsG_{season}.csv")
